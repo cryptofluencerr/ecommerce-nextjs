84e1abd87f2a219a056f5dfe98fe236f4ad165ab
kcpython <kcbizzkc@gmail.com>
2022-08-10 16:38:38 +0530

    change in next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    MONGO_URI: process.env.MONGO_URI,
+    URL: process.env.URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
